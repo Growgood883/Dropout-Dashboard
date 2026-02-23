3786181756df6c3a2ddf91568f861f8e9c1a14e6
Yukti Shah <growgood883@gmail.com>
2026-02-23 11:50:24 +0530

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(
 )
 
 # LOAD DATA
-df = pd.read_csv("student_risk_dashboard(1).csv")
+df = pd.read_csv("student_risk_dashboard.csv")
 
 st.title("🎓 Gujarat School Dropout Decision Support System")
 st.markdown(
